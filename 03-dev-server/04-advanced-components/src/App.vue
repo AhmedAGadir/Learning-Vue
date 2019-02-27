6299bb3a6cbc68c0a1378760e8f09425276ff852
AhmedAGadir <ahmed.gadir@gmail.com>
2019-02-27 11:05:30 +0000

    update

---
+++ 03-dev-server/04-advanced-components/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- passing props without binding (not dynamic): -->
+        <!-- <app-quote quote="a wONDERFUL qUOTE"> -->
+        <!-- passing props with binding (dynamic): -->
+        <!-- <app-quote :quote="quote"> -->
+        <!-- ************************************************************** -->
+        <!-- ************************************************************** -->
+        <!-- ************************************************************** -->
+        <!-- passing HTML props through children (useful for when you want to distribute content in other components e.g. widgets, slideshows etc): -->
+        <!-- when only using a single slot: -->
+        <!-- <app-quote>
+          <h2>tHE qUOTE</h2>
+          <p>{{ quote }}</p>
+        </app-quote>-->
+        <!-- when you want to use multiple slots -->
+        <!-- <app-quote>
+          <h2 slot="title">tHE qUOTE</h2>
+          <p slot="content">{{ quote }}</p>
+        </app-quote>-->
+        <!-- everything that doesnt have a slot attribute will be part of the default slot -->
+        <!-- <app-quote>
+          <h2 slot="title">tHE qUOTE</h2>
+          <p>{{ quote }}</p>
+        </app-quote>-->
+        <!-- ************************************************************** -->
+        <!-- ************************************************************** -->
+        <!-- ************************************************************** -->
+        <!-- switching multiple components with dynamic components -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote.vue";
+import appAuthor from "./components/Author.vue";
+import appNew from "./components/New.vue";
+export default {
+  // remember in proper project setups, data must be a function that returns an object!
+  data: () => ({
+    quote: "a wONDERFUL qUOTE"
+  }),
+  components: {
+    appQuote: Quote,
+    appAuthor,
+    appNew
+  }
+};
+</script>
+
+<style scoped>
+/* slots aka children be styled in the component they are defined in (when styling is scoped) */
+h2 {
+  text-decoration: underline;
+}
+</style>
---
+++ 03-dev-server/04-advanced-components/src/components/Quote.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- children is passed through reserved tag 'slot' -->
+    <!-- when only using one slot -->
+    <!-- <slot></slot> -->
+    <!-- when using multiple slots - use named slots -->
+    <!-- <slot name="title"></slot>
+    <slot name="content"></slot>-->
+    <!-- mixing named and default slots: -->
+    <slot name="title"></slot>
+    <slot></slot>
+    <!-- in the case where we have a named slot that may or may not be passed down, 
+    we can insert some fallback content between the slot tags to be displayed-->
+    <span style="color: #ccc">
+      <slot name="subtitle">The Subtitle</slot>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 2px black;
+  padding: 30px;
+  margin: 30px auto;
+  text-align: center;
+}
+/* slots can also be styled in here */
+h2 {
+  color: blue;
+}
+</style>
