a9102d37f8c65a8dcf6089c56dffb8027022ff7d
AhmedAGadir <ahmed.gadir@gmail.com>
2019-03-18 10:55:22 +0000

    update

--- 07-http-with-vue/02-default-urls-interceptors-and-custom-resources/src/App.vue
+++ 07-http-with-vue/02-default-urls-interceptors-and-custom-resources/src/App.vue
@@ -17,6 +17,9 @@
     <hr>
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <input type="text" class="form-control" v-model="node">
+        <br>
+        <br>
         <button class="btn btn-primary" @click="fetchData">Get Data</button>
         <br>
         <br>
@@ -39,28 +42,60 @@ export default {
       username: "",
       email: ""
     },
-    users: []
+    users: [],
+    resource: {},
+    node: "data"
   }),
   methods: {
     submit() {
       // we can configure a default url in our application - see main.js
-      this.$http
-        .post("data.json", this.user)
-        .then(response => {
-          console.log(response);
+      // this.$http
+      //   .post("data.json", this.user)
+      //   .then(response => {
+      //     console.log(response);
+      //   })
+      //   .catch(err => {
+      //     console.log(err);
+      //   });
+
+      // save is one of the default actions provided by vueresource
+      // first argument is paramateres we may want to pass
+      // second argument is data we want to pass
+
+      // without dynamic url configured
+      // this.resource.save({}, this.user);
+      // with dynamic url configured
+      this.resource.save({ node: this.node }, this.user);
+      // or
+      // this.resource.saveAlt(this.user);
+    },
+    fetchData() {
+      // this.$http
+      //   .get("data.json")
+      //   .then(response => response.json())
+      //   .then(data => {
+      //     this.users = Object.keys(data).map(key => data[key]);
+      //   });
+      this.resource
+        .getData({ node: this.node })
+        .then(response => response.json())
+        .then(data => {
+          this.users = Object.keys(data).map(key => data[key]);
         })
         .catch(err => {
           console.log(err);
         });
-    },
-    fetchData() {
-      this.$http
-        .get("data.json")
-        .then(response => response.json())
-        .then(data => {
-          this.users = Object.keys(data).map(key => data[key]);
-        });
     }
+  },
+  created() {
+    // initialise resources
+    const customActions = {
+      saveAlt: { method: "POST", url: "alternative.json" },
+      getData: { method: "GET" }
+    };
+    // this.resource = this.$resource("data.json", {}, customActions);
+    // dynamically setting the endpooint
+    this.resource = this.$resource("{node}.json", {}, customActions);
   }
 };
 </script>
