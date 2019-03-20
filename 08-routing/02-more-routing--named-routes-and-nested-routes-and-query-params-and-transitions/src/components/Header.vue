42bbf5736041b7ba6cc03f3a0105bd0c5adb054e
AhmedAGadir <ahmed.gadir@gmail.com>
2019-03-20 10:43:06 +0000

    update

---
+++ 08-routing/02-more-routing--named-routes-and-nested-routes-and-query-params-and-transitions/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <ul class="nav nav-pills">
+    <router-link to="/" tag="li" active-class="active" exact>
+      <a>Home</a>
+    </router-link>
+    <router-link to="/user" tag="li" active-class="active">
+      <a>User</a>
+    </router-link>
+    <!-- if you wanted to you could set up query params in the 'to' property e.g. -->
+    <!-- <router-link to="/user?a=100&b='hello'" tag="li" active-class="active"><a>User</a></router-link> -->
+  </ul>
+</template>
---
+++ 08-routing/02-more-routing--named-routes-and-nested-routes-and-query-params-and-transitions/src/components/user/User.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>The User Page</h1>
+    <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
+    <hr>
+    <!-- since we confiugred this component with children routes, we need to use the <router-view> component here -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateToHome() {
+      // looks clean
+      this.$router.push({ name: "home" });
+    }
+  }
+};
+</script>
